dbefc55fc266a752dd4a025bd3c84b0d607522a1
DanPaul12 <dan.paul.schechter@gmail.com>
2024-08-03 21:00:46 -0700

    hw20

---
+++ practice.py
@@ -0,0 +1,15 @@
+def main():
+    while True:
+        command = input(f"Welcome to the Library Management System!\nMain Menu:\n1. Book Operations\n2. User Operations\n3. Author Operations\n4. Genre Operations\n5. Quit")
+        if command == 1:
+            pass
+        elif command == 2:
+            pass
+        elif command == 3:
+            pass
+        elif command == 4:
+            pass
+        elif command == 5:
+            pass
+        else:
+            print("Invalid command")
---
+++ user_interface.py
@@ -0,0 +1,52 @@
+class UserInterface:
+    def book_operations():
+        while True:
+            command = int(input(f"Book Operations:\n1. Add a new book\n2. Borrow a book\n3. Return a book\n4. Search for a book\n5. Display all books"))
+            if command == 1:
+                pass
+            elif command == 2:
+                pass
+            elif command == 3:
+                pass
+            elif command == 4:
+                pass
+            elif command == 5:
+                pass
+            else:
+                print("Invalid command")
+
+    def user_operations():
+        while True:
+            command = int(input(f"User Operations:\n1. Add a new user\n2. View user details\n3. Display all users"))
+            if command == 1:
+                pass
+            elif command == 2:
+                pass
+            elif command == 3:
+                pass
+            else:
+                print("Invalid command")
+
+    def author_operations():
+        while True:
+            command = int(input(f"Author Operations:\n1. Add a new author\n2. View author details\n3. Display all authors"))
+            if command == 1:
+                pass
+            elif command == 2:
+                pass
+            elif command == 3:
+                pass
+            else:
+                print("Invalid command")
+
+    def genre_operations():
+        while True:
+            command = int(input(f"Genre Operations:\n1. Add a new genre\n2. View genre details\n3. Display all genres"))
+            if command == 1:
+                pass
+            elif command == 2:
+                pass
+            elif command == 3:
+                pass
+            else:
+                print("Invalid command")
