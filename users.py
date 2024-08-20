9e680dafb4dcb5548104925bb468d75416dd9ae9
DanPaul12 <dan.paul.schechter@gmail.com>
2024-08-20 15:48:33 -0700

    hw20

--- users.py
+++ users.py
@@ -1,12 +1,26 @@
+from sql_connect import connect_database
+
 class Users:
     def __init__(self):
         self.users = {}
  
     def add_user(self):
-        name = input("What is user's name?: ")
-        libID = input("What is user's id number?: ")
-        self.users[name]= {"name":name, "Library ID":libID}
-        print(self.users[name])
+        conn = connect_database()
+        if conn is not None:
+            try:
+                cursor = conn.cursor()
+                user_id = int(input("What is the user id?: "))
+                name = input("What is user's name?: ")
+                libID = input("What is user's id number?: ")
+                query = "INSERT INTO users Values (%s, %s, %s)"
+                values = user_id, name, libID
+                cursor.execute(query, values)
+                conn.commit()
+                print("User added succesfully")
+            finally:
+                cursor.close()
+                conn.close()
+                
         
     def search_users(self):
         user = input("What is the name of the user you're searching for?")
